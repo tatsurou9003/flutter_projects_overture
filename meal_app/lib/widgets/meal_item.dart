25ba248bbfb3f127539bf1e29911d5e91f15ed3b
tatsurou9003 <t.murakami@4nonome.com>
2024-04-10 09:35:52 +0900

    screen

---
+++ meal_app/lib/widgets/meal_item.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/models/meal.dart';
+
+class MealItem extends StatelessWidget {
+  const MealItem({super.key, required this.meal});
+
+  final Meal meal;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
