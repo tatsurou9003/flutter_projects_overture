8fc05f6eefe518a65456fc31c3e2a95574da28d1
tatsurou9003 <t.murakami@4nonome.com>
2024-04-26 00:49:54 +0900

    auth validation

--- chat_app/lib/screens/auth.dart
+++ chat_app/lib/screens/auth.dart
@@ -8,73 +8,119 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _form = GlobalKey<FormState>();
+  var _isLogin = true;
+
+  var _enteredEmail = '';
+  var _enteredPassword = '';
+
+  void _submit() {
+    final isValid = _form.currentState!.validate();
+    if (!isValid) {
+      return;
+    }
+    _form.currentState!.save();
+
+    print(_enteredEmail);
+    print(_enteredPassword);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                margin: const EdgeInsets.only(
-                  top: 30,
-                  bottom: 20,
-                  left: 20,
-                  right: 20,
-                ),
-                width: 200,
-                child: Image.asset(
-                  'assets/images/chat.png',
-                ),
+    return GestureDetector(
+      onTap: FocusScope.of(context).unfocus,
+      child: Scaffold(
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          body: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(
+                      top: 30,
+                      bottom: 20,
+                      left: 20,
+                      right: 20,
+                    ),
+                    width: 200,
+                    child: Image.asset(
+                      'assets/images/chat.png',
+                    ),
+                  ),
+                  Card(
+                    margin: const EdgeInsets.all(20),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Form(
+                          key: _form,
+                          child: Column(
+                            children: [
+                              TextFormField(
+                                decoration: const InputDecoration(
+                                    labelText: 'Email Address'),
+                                keyboardType: TextInputType.emailAddress,
+                                autocorrect: false,
+                                textCapitalization: TextCapitalization.none,
+                                validator: (value) => value == null ||
+                                        value.trim().isEmpty ||
+                                        !value.contains('@')
+                                    ? 'Please enter a valid email address'
+                                    : null,
+                                onSaved: (value) {
+                                  _enteredEmail = value!;
+                                },
+                              ),
+                              TextFormField(
+                                decoration: const InputDecoration(
+                                    labelText: 'Password'),
+                                obscureText: true,
+                                validator: (value) => value == null ||
+                                        value.trim().length < 6
+                                    ? 'Password must be at least 7 characters long'
+                                    : null,
+                                onSaved: (value) {
+                                  _enteredPassword = value!;
+                                },
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              ElevatedButton(
+                                onPressed: _submit,
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Theme.of(context)
+                                      .colorScheme
+                                      .primaryContainer,
+                                ),
+                                child: Text(_isLogin ? 'Login' : 'Signup'),
+                              ),
+                              const SizedBox(
+                                height: 8,
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  setState(() {
+                                    _isLogin = !_isLogin;
+                                  });
+                                },
+                                style: TextButton.styleFrom(
+                                  backgroundColor: Theme.of(context)
+                                      .colorScheme
+                                      .primaryContainer,
+                                ),
+                                child: Text(_isLogin
+                                    ? 'Create new account'
+                                    : 'I already have an account'),
+                              )
+                            ],
+                          )),
+                    ),
+                  )
+                ],
               ),
-              Card(
-                margin: const EdgeInsets.all(20),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Form(
-                      child: Column(
-                    children: [
-                      TextFormField(
-                        decoration:
-                            const InputDecoration(labelText: 'Email Address'),
-                        keyboardType: TextInputType.emailAddress,
-                        autocorrect: false,
-                        textCapitalization: TextCapitalization.none,
-                      ),
-                      TextFormField(
-                        decoration:
-                            const InputDecoration(labelText: 'Password'),
-                        obscureText: true,
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor:
-                              Theme.of(context).colorScheme.primaryContainer,
-                        ),
-                        child: const Text('Login'),
-                      ),
-                      const SizedBox(
-                        height: 8,
-                      ),
-                      TextButton(
-                        onPressed: () {},
-                        style: TextButton.styleFrom(
-                          backgroundColor:
-                              Theme.of(context).colorScheme.primaryContainer,
-                        ),
-                        child: const Text('Create new account'),
-                      )
-                    ],
-                  )),
-                ),
-              )
-            ],
-          ),
-        ));
+            ),
+          )),
+    );
   }
 }
