df3cb4f1963a46ab4441bf2ac8acc31d483961a0
tatsurou9003 <t.murakami@4nonome.com>
2024-04-29 23:24:58 +0900

    image_picker

---
+++ chat_app/lib/widgets/user_image_picker.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+
+class UserImagePicker extends StatefulWidget {
+  const UserImagePicker({super.key});
+
+  @override
+  State<UserImagePicker> createState() => _UserImagePickerState();
+}
+
+class _UserImagePickerState extends State<UserImagePicker> {
+  File? _pickedImageFile;
+
+  void _pickImage() async {
+    final imagePicker = await ImagePicker().pickImage(
+      source: ImageSource.camera,
+      imageQuality: 50,
+      maxWidth: 150,
+    );
+
+    if (imagePicker == null) {
+      return;
+    }
+
+    setState(() {
+      _pickedImageFile = File(imagePicker.path);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 40,
+          backgroundColor: Colors.grey,
+          foregroundImage:
+              _pickedImageFile != null ? FileImage(_pickedImageFile!) : null,
+        ),
+        TextButton.icon(
+          onPressed: _pickImage,
+          icon: const Icon(Icons.image),
+          label: Text(
+            'Add Image',
+            style: TextStyle(color: Theme.of(context).colorScheme.primary),
+          ),
+        )
+      ],
+    );
+  }
+}
