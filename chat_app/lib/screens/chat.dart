2ccd78bc3e19b18eedd9234828cf16ca24329ea1
tatsurou9003 <t.murakami@4nonome.com>
2024-05-02 09:22:47 +0900

    puch cloudFunction

--- chat_app/lib/screens/chat.dart
+++ chat_app/lib/screens/chat.dart
@@ -2,11 +2,15 @@ import 'package:chat_app/widgets/chat_messages.dart';
 import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
 
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +21,7 @@ class ChatScreen extends StatelessWidget {
               onPressed: () {
                 FirebaseAuth.instance.signOut();
               },
-              icon: Icon(Icons.exit_to_app),
+              icon: const Icon(Icons.exit_to_app),
               color: Theme.of(context).colorScheme.primary,
             )
           ],
